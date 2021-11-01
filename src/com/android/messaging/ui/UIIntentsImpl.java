ae0eb8f1854a2b49636a59af616922f6f7abdcf6
Maitreya29 <maitreyapatni30@gmail.com>
2021-11-01 18:07:13 +0530

    Messaging: Adapt PendingIntent to target Android 12.

--- src/com/android/messaging/ui/UIIntentsImpl.java
+++ src/com/android/messaging/ui/UIIntentsImpl.java
@@ -427,7 +427,7 @@ public class UIIntentsImpl extends UIIntents {
         }
         return PendingIntent.getBroadcast(context,
                 requestCode, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent.FLAG_MUTABLE);
     }
 
     /**
@@ -445,7 +445,7 @@ public class UIIntentsImpl extends UIIntents {
         // Adds the back stack for the Intent (plus the Intent itself)
         stackBuilder.addNextIntentWithParentStack(intent);
         final PendingIntent resultPendingIntent =
-            stackBuilder.getPendingIntent(requestCode, PendingIntent.FLAG_UPDATE_CURRENT);
+            stackBuilder.getPendingIntent(requestCode, PendingIntent.FLAG_MUTABLE);
         return resultPendingIntent;
     }
 
@@ -458,7 +458,7 @@ public class UIIntentsImpl extends UIIntents {
                 getSmsStorageLowWarningActivityIntent(context));
 
         return taskStackBuilder.getPendingIntent(
-                0, PendingIntent.FLAG_UPDATE_CURRENT);
+                0, PendingIntent.FLAG_MUTABLE);
     }
 
     @Override
